594df6b3ddb66f4dcefa80aa577277b089e22980
Hieu X. Leu <hieuleuxuan@gmail.com>
2023-06-10 21:59:11 +1000

    Initial Commit

---
+++ Metaball/MetaballApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaballApp.swift
+//  Metaball
+//
+//  Created by Hieu Xuan Leu on 10/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct MetaballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
